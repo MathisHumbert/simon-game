9f443b8e38e089f56240f5850581136b498738d4
MathisHumbert <84037244+MathisHumbert@users.noreply.github.com>
2021-09-02 18:53:20 +0200

    js added

--- app.js
+++ app.js
@@ -4,30 +4,27 @@ const start = document.querySelector('.play');
 
 // initilize var
 let memory = [];
+let counter = 0;
 let startFlag = true;
+let showFlag = true;
 
 // event
 start.addEventListener('click', launchGame);
 panels.forEach((panel) => {
-  panel.addEventListener('click', () => {
-    displayColor(panel);
-  });
-  // panel.addEventListener('click');
+  panel.addEventListener('click', userResponse);
 });
 
+// start the game
 function launchGame() {
-  // if (!startFlag) return;
+  if (!startFlag) return;
   let random = Math.floor(Math.random() * 4 + 1);
   memory.push(random);
-  memory.forEach((item, i) =>
-    setTimeout(() => {
-      panels.forEach((panel) => {
-        if (panel.dataset.id == item) {
-          displayColor(panel);
-        }
-      });
-    }, i * 1000)
-  );
+  panels.forEach((panel) => {
+    if (panel.dataset.id == random) {
+      displayColor(panel);
+    }
+  });
+  startFlag = false;
 }
 
 // display the color
@@ -38,12 +35,50 @@ function displayColor(panel) {
   }, 500);
 }
 
-// if (!startFlag) return;
-// let random = Math.floor(Math.random() * 4 + 1);
-// memory.push(random);
-// panels.forEach((panel) => {
-//   if (panel.dataset.id == random) {
-//     displayColor(panel);
-//   }
-// });
-// startFlag = false;
+// user playing the game
+function userResponse() {
+  // if the start button has not been pressed
+  if (startFlag) return;
+  // if the game is showing the newt moves
+  if (!showFlag) return;
+
+  displayColor(this);
+
+  // if the player is wrong
+  if (!(this.dataset.id == memory[counter])) {
+    console.log('error');
+    return;
+  }
+
+  // if the player is right and if he has done all of the steps
+  if (counter + 1 == memory.length) {
+    showFlag = false;
+
+    // launch a new set
+    setTimeout(() => {
+      // adding a new item to the memory
+      let random = Math.floor(Math.random() * 4 + 1);
+      memory.push(random);
+
+      // displaying all of the actual memory in HTML
+      memory.forEach((item, i) =>
+        setTimeout(() => {
+          panels.forEach((panel) => {
+            if (panel.dataset.id == item) {
+              displayColor(panel);
+            }
+            if (i + 1 == memory.length) {
+              showFlag = true;
+            }
+          });
+        }, i * 1250)
+      );
+    }, 2000);
+    counter = 0;
+  }
+
+  // if the player is right but has not done all of the steps
+  else {
+    counter++;
+  }
+}
